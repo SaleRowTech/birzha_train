c9d1a0e6f5ef1227d229b4d2526d3601c6f1f18c
vlazaay <vladyslav.zaiets@gmail.com>
2021-07-22 15:41:07 +0300

    test

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -14,6 +14,9 @@ use Symfony\Component\Security\Core\User\UserInterface;
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
+    const ROLE_DEFAULT = 'BROKER';
+
+    const ROLE_LEGAL = 'LEGAL';
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -84,7 +87,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        $roles[] = static::ROLE_DEFAULT;
 
         return array_unique($roles);
     }
