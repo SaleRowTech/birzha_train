8b7128c9ea3eea1312ddefbe9d12a21f5df33cbb
vlazaay <vladyslav.zaiets@gmail.com>
2021-07-28 17:10:19 +0300

    auction start

--- src/Controller/AuctionController.php
+++ src/Controller/AuctionController.php
@@ -130,7 +130,7 @@ class AuctionController extends AbstractController
         $bets = json_decode($auction->getBets());
 
         return $this->render('auction/bet/bets.html.twig', [
-            'bets' => $bets,
+            'bets' => $bets[0],
         ]);
     }
 
