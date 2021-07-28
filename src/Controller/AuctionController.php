8c66eba15241dcf720fec47f3f8c7aa61f8e5a87
vlazaay <vladyslav.zaiets@gmail.com>
2021-07-28 15:48:14 +0300

    auction start

--- src/Controller/AuctionController.php
+++ src/Controller/AuctionController.php
@@ -83,7 +83,7 @@ class AuctionController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             //here will be method, that saving data from form in base(array)
             $data = $form->getData();
-            $date = new \DateTime("Y-m-d H:i:s");
+            $date = new \DateTime('@'.strtotime('now'));
             $auction->setBets([
                 "bet" => $data['bet'],
                 "date"=> $date,
